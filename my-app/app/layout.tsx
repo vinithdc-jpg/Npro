ed1e4b044e8d4dee0fe84d8a08171f22002ad5ea
vinith <vinithdc@gmail.com>
2026-08-17 19:28:53 +0530

    navbar work

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +23,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <NavBar />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
